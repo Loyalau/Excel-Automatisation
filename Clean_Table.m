13b69662503c068011fee67297bda5ef3c467a85
sonia <sonia@LAPTOP-3UI33UIV>
2026-07-03 11:50:37 +0200

    Commit sans untracked files

---
+++ Clean_Table.m
@@ -0,0 +1,18 @@
+%This function aim to clean the table created by Open_Excel, to do so we
+%wants to delete all the row before the offset zero of the test.
+function [outputArg1,outputArg2] = Clean_Table(inputArg1,inputArg2)
+%CLEAN_TABLE undefined
+%   undefined
+arguments (Input)
+    inputArg1
+    inputArg2
+end
+
+arguments (Output)
+    outputArg1
+    outputArg2
+end
+
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
---
+++ Open_Excel.m
@@ -0,0 +1,18 @@
+%This function open the Excel file and write as an arguments write a table
+%with all the important result of the file
+function [outputArg1,outputArg2] = Open_Excel(inputArg1,inputArg2)
+%OPEN_EXCEL undefined
+%   undefined
+arguments (Input)
+    inputArg1
+    inputArg2
+end
+
+arguments (Output)
+    outputArg1
+    outputArg2
+end
+
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
